79cb5a3f5353ca27916a1a5bffd68e61279dd58f
usagle <info@usagle.com>
2018-08-02 15:03:57 +0300

    Fix: Modified config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,5 +18,24 @@ namespace :deploy do
     end
   end
 
+  desc 'Remote console'
+  task :console do
+    on roles(:app) do |h|
+      run_interactively "bundle exec rails console #{fetch(:rails_env)}", h.user
+    end
+  end
+
+  desc 'Remote dbconsole'
+  task :dbconsole do
+    on roles(:app) do |h|
+      run_interactively "bundle exec rails dbconsole #{fetch(:rails_env)}", h.user
+    end
+  end
+
+  def run_interactively(command, user)
+    info "Running `#{command}` as #{user}@#{host}"
+    exec %(ssh #{user}@#{host} -t "bash --login -c 'cd #{fetch(:deploy_to)}/current && #{command}'")
+  end
+
   after :publishing, :restart
 end
