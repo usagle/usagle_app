4df515fd33c2de7d6d04606bbb81ff1ab1ea93b9
usagle <info@usagle.com>
2018-08-02 16:52:17 +0300

    Fix: Changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,16 +22,10 @@ namespace :deploy do
 end
 
 namespace :rails do
-  desc 'Open a rails console `cap [staging] rails:console [server_index default: 0]`'
-  task :console do
-    server = roles(:app)[ARGV[2].to_i]
-
-    puts "Opening a console on: #{server.hostname}…."
-
-    cmd = "ssh #{server.user}@#{server.hostname} -t 'cd #{fetch(:deploy_to)}/current && RAILS_ENV=#{fetch(:rails_env)} bundle exec rails console'"
-
-    puts cmd
-
-    exec cmd
+  desc 'Open the rails console on one remote server'
+  task :console, roles: :app do
+    hostname = find_servers_for_task(current_task).first
+    port = exists?(:port) ? fetch(:port) : 22
+    exec "ssh -l #{user} #{hostname} -p #{port} -t 'source ~/.profile && #{current_path}/script/rails c #{rails_env}'"
   end
 end
