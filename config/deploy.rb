c74b60dd257f61e24e25f7b78c578bd5288f5b8e
usagle <info@usagle.com>
2018-08-02 16:59:24 +0300

    Fix: Change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,12 +20,3 @@ namespace :deploy do
 
   after :publishing, :restart
 end
-
-namespace :rails do
-  desc 'Open the rails console on one remote server'
-  task :console, roles: :app do
-    hostname = find_servers_for_task(current_task).first
-    port = exists?(:port) ? fetch(:port) : 22
-    exec "ssh -l #{user} #{hostname} -p #{port} -t 'source ~/.profile && #{current_path}/script/rails c #{rails_env}'"
-  end
-end
